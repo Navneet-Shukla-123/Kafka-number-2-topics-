c5363e4d08e52af087673811de534a92b11280b5
Navneet-Shukla-123 <Navneet.shukla@pantelwar.com>
2024-07-15 13:51:19 +0530

    Working with 2 topics in kafka

---
+++ consumer/consumer.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+const (
+	ConsumerGroup      = "number-group"
+	ConsumerTopicEven  = "even"
+	ConsumerTopicOdd   = "odd"
+	KafkaServerAddress = "localhost:9092"
+)
+
+type Number map[string][]int
+
+type NumberStore struct {
+	data Number
+	mu   sync.Mutex
+}
+
+func (ns *NumberStore) Add(value int) {
+	ns.mu.Lock()
+	if value%2 == 0 {
+		ns.data["even"] = append(ns.data["even"], value)
+	} else {
+		ns.data["odd"] = append(ns.data["odd"], value)
+
+	}
+
+	ns.mu.Unlock()
+
+}
+
+func (ns *NumberStore) GetEven() []int {
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	return ns.data["even"]
+}
+
+func (ns *NumberStore) GetOdd() []int {
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	return ns.data["odd"]
+}
+
+type Consumer struct {
+	store *NumberStore
+}
+
+func (*Consumer) Setup(sarama.ConsumerGroupSession) error   { return nil }
+func (*Consumer) Cleanup(sarama.ConsumerGroupSession) error { return nil }
+
+func (consumer *Consumer) ConsumeClaim(
+	sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim,
+) error {
+	for msg := range claim.Messages() {
+		key := string(msg.Key)
+		log.Println("Key is ", key)
+		value, err := strconv.Atoi(string(msg.Value))
+		if err != nil {
+			log.Println("error convertint he value to int ", err)
+			continue
+		}
+
+		log.Println("value is ", value)
+		consumer.store.Add(value)
+		sess.MarkMessage(msg, "")
+	}
+	return nil
+}
+
+func initializeConsumerGroup() (sarama.ConsumerGroup, error) {
+	config := sarama.NewConfig()
+
+	consumerGroup, err := sarama.NewConsumerGroup(
+		[]string{KafkaServerAddress}, ConsumerGroup, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize consumer group: %w", err)
+	}
+
+	return consumerGroup, nil
+}
+
+func setupConsumerGroup(ctx context.Context, store *NumberStore) {
+	consumerGroup, err := initializeConsumerGroup()
+	if err != nil {
+		log.Printf("initialization error for consumer group: %v", err)
+	}
+
+	defer consumerGroup.Close()
+
+	consumer := &Consumer{store: store}
+	topics := []string{
+		ConsumerTopicEven, ConsumerTopicOdd,
+	}
+
+	for {
+		err = consumerGroup.Consume(ctx, topics, consumer)
+		if err != nil {
+			log.Printf("error from consumer: %v", err)
+		}
+		if ctx.Err() != nil {
+			return
+		}
+	}
+}
+
+func getData(store *NumberStore) {
+	for {
+		dataEven := store.GetEven()
+		dataOdd := store.GetOdd()
+
+		log.Println("Even number is ")
+		log.Println(dataEven)
+
+		log.Println("Odd number is ")
+		log.Println(dataOdd)
+
+		time.Sleep(20 * time.Second)
+	}
+}
+
+func main() {
+	store := &NumberStore{
+		data: make(Number),
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	go setupConsumerGroup(ctx, store)
+	defer cancel()
+
+	getData(store)
+}
---
+++ producer/producer.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+const (
+	KafkaServerAddress = "localhost:9092"
+	KafkaTopicEven     = "even"
+	KafkaTopicOdd      = "odd"
+)
+
+func sendKafkaMessage(producer sarama.SyncProducer, val int, topic string) error {
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Key:   sarama.StringEncoder("number"),
+		Value: sarama.StringEncoder(strconv.Itoa(val)),
+	}
+
+	_, _, err := producer.SendMessage(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func sendMessageFunction(producer sarama.SyncProducer) {
+	log.Println("Producer is started successfully")
+	i := 1
+	for {
+
+		var topic string
+
+		if i%2 == 0 {
+			topic = KafkaTopicEven
+
+		} else {
+			topic = KafkaTopicOdd
+		}
+
+		err := sendKafkaMessage(producer, i, topic)
+		if err != nil {
+			log.Println("producer is closed successfully")
+			return
+		}
+		i++
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func setupProducer() (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	producer, err := sarama.NewSyncProducer([]string{KafkaServerAddress},
+		config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to setup producer: %w", err)
+	}
+	return producer, nil
+}
+
+func main() {
+	producer, err := setupProducer()
+	if err != nil {
+		log.Println("error in setting up the server ", err)
+		return
+	}
+
+	defer producer.Close()
+
+	sendMessageFunction(producer)
+}
